36a01a221cfdc0fb3b9c8f36345f181f368f0c18
srtidd23 <sean.tidd@revature.net>
2020-10-12 15:03:00 -0400

    added csv header ignore

--- src/main/scala/Entities/DBConnection.scala
+++ src/main/scala/Entities/DBConnection.scala
@@ -4,19 +4,23 @@ package Entities
 /** DBConnection class handles all methods that interact with the database */
 
 class DBConnection(username: String, password: String) {
-  def uploadCSV(path: String): Unit ={
+  def uploadCSV(path: String): Unit = {
     val source = io.Source.fromFile(path)
-    for (line <- source.getLines()){
-      val cols = line.split(",")
-      cols match{
-        case Array(name, numPlayers, hours, rank) => println(s"This board game is called $name, it is played with $numPlayers players for an average play time of $hours hours. It is ranked $rank")
+    var header = true
+    for (line <- source.getLines()) {
+      if (!header) {
+        val cols = line.split(",")
+        cols match {
+          case Array(name, numPlayers, hours, rank) => println(s"This board game is called $name, it is played with $numPlayers players for an average play time of $hours hours. It is ranked $rank")
+        }
+        // Upload columns to database
       }
-      // Upload columns to database
+      header = false
     }
   }
 }
-
 object CSVTest extends App{
   val uploadCSV = new DBConnection("df", "df")
-  uploadCSV.uploadCSV("C:\\Users\\river\\IdeaProjects\\Tidd_Project0\\src\\main\\scala\\Entities\\test.csv")
+  uploadCSV.uploadCSV("C:\\Users\\river\\IdeaProjects\\Tidd_Project0\\src\\main\\scala\\test.csv")
 }
+
